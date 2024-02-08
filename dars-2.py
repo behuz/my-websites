d89c9034db90680a1554d5d4859c19217f745ff8
Behruz <xoliqberdiyevbehruz@gmail.com>
2024-02-08 21:00:40 +0500

    new files

---
+++ dars-2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  5 21:53:04 2024
+
+@author: Behruz
+"""
+
+#print('"Tico","Nexia", "Damas" korganlar qilar havas')
+print(5**4)
+print(22%4)
+print(125*125)
+print(125*4)
